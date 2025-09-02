5eb5806cb78811403fd80c181d8258088f386920
Nilton Filho <niltonfilho115@gmail.com>
2025-09-02 12:34:36 -0300

    Exercicios sobre tipos primitivos

---
+++ src/academy/devdojo/maratonajavadevdojo/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajavadevdojo.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Nilton";
+        String endereco = "Rua Rio Preto, 310";
+        double salario = 2300.50;
+        String data = "01/09/2025";
+        String relatorio = "Eu, " + nome + ", morando no endereço " + endereco + ", confirmo que recebi o salário de R$" + salario + " na data " + data;
+
+        System.out.println(relatorio);
+    }
+}

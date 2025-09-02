2504418e2acccf498b3c91281175ca54777248a6
Nilton Filho <niltonfilho115@gmail.com>
2025-09-02 12:00:50 -0300

    Aulas iniciais, entendendo o ambiente

---
+++ src/academy/devdojo/maratonajavadevdojo/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajavadevdojo.introducao;
+
+public class Aula01OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("KA-ME-HA-ME-HAAA");
+    }
+}

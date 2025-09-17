b261af4a1903150105ec7b15af0f53a43afaed4b
Nilton Filho <niltonfilho115@gmail.com>
2025-09-17 02:00:28 -0300

    Operadoes em java

---
+++ src/academy/devdojo/maratonajavadevdojo/introducao/Aula04Operadores.java
@@ -0,0 +1,58 @@
+package academy.devdojo.maratonajavadevdojo.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        int numero01 = 10;
+        int numero02 = 20;
+        double resultado = numero01 / (double) numero02;
+        System.out.println(resultado);
+
+        int resto = 21 % 7;
+        System.out.println(resto);
+
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10.0;
+        boolean isDezDiferenteDez = 10 != 20;
+        System.out.println("isDezMaiorQueVinte " + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte " + isDezIgualVinte);
+        System.out.println("isDezIgualDez " + isDezIgualDez);
+        System.out.println("isDezDiferenteDez " + isDezDiferenteDez);
+
+        int idade = 29;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+        System.out.println("isDentroDaLeiMaiorQueTrinta " + isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta " + isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPS = 5000F;
+        boolean isPSCincoCompravel = valorTotalContaCorrente > valorPS || valorTotalContaPoupanca > valorPS;
+        System.out.println("isPSCincoCompravel " + isPSCincoCompravel);
+
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println("bonus " + bonus);
+
+        int contador = 0;
+        contador++;
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        System.out.println("contador " + contador);
+        int contador2 = 0;
+        System.out.println("contador2 " + contador2++);
+        System.out.println("contador2 " + contador2);
+        System.out.println("contador2 " + ++contador2);
+
+    }
+}

4caf8f5a1c883ada0b52a6dd9584eee9d74c02a7
niltinfilho <niltonfilho115@gmail.com>
2025-10-06 11:21:04 -0300

    comentarios em java

--- src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,13 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
 public class OlaDevDojo {
+    // Isso é um comentário simples
+    /*
+       Isso é um comentário de múltiplas linhas
+    */
     public static void main(String[] args) {
         System.out.println("Olá, mundo!");
     }

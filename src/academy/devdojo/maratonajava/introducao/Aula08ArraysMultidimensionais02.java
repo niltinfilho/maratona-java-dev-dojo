70cf69315639d9eb50179c52993af20cda547d4d
niltinfilho <niltonfilho115@gmail.com>
2025-10-07 10:01:28 -0300

    entendendo arrays multidimensionais

---
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais02.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula08ArraysMultidimensionais02 {
+    public static void main(String[] args) {
+        int[][] dias = new int[3][3];
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 31;
+
+        dias[1][0] = 31;
+        dias[1][1] = 28;
+        dias[1][2] = 31;
+
+        for (int [] arrBase: dias) {
+            for (int num: arrBase) {
+                System.out.println(num);
+            }
+        }
+    }
+}

99282476d1b91e66b501bc5a7e1fa83674d136cb
niltinfilho <niltonfilho115@gmail.com>
2025-10-08 14:30:01 -0300

    teste - corrigindo commits

--- src/academy/devdojo/maratonajava/javacore/Isobrescrita/teste/AnimeTeste01.java
+++ src/academy/devdojo/maratonajava/javacore/Isobrescrita/teste/AnimeTeste01.java
@@ -5,7 +5,6 @@ import academy.devdojo.maratonajava.javacore.Isobrescrita.dominio.Anime;
 public class AnimeTeste01 {
     public static void main(String[] args) {
         Anime anime = new Anime("Dr. Stone");
-
         System.out.println(anime);
     }
 }

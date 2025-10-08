9dc04d17b0444b42e701c7dbbdbbc04888e0d619
niltinfilho <niltonfilho115@gmail.com>
2025-10-08 14:13:21 -0300

    entendendo sequencia de inicializacao

---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/teste/HerancaTeste02.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.teste;
+
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Funcionario;
+
+public class HerancaTeste02 {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("Jiraya");
+    }
+}

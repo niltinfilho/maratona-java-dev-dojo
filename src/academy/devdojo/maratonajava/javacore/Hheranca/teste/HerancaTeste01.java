f8fc569692eeb455f794f403292415f053b0264c
niltinfilho <niltonfilho115@gmail.com>
2025-10-08 13:31:10 -0300

    iniciando o entendimento em herança

---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/teste/HerancaTeste01.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.teste;
+
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Endereco;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTeste01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("012345-678");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Jorge");
+        pessoa.setCpf("111111111-11");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+        System.out.println("----------------");
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Oda Nobunaga");
+        funcionario.setCpf("123456789-00");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(20000);
+
+        funcionario.imprime();
+    }
+}

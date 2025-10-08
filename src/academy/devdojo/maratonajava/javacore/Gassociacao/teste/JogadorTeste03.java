8374efc3e1cdfe19fe2162e8f029bddacc781a83
niltinfilho <niltonfilho115@gmail.com>
2025-10-08 10:14:06 -0300

    entendendo associacao bidirecional

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/teste/JogadorTeste03.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.teste;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Jogador;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Time;
+
+public class JogadorTeste03 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Cafu");
+        Jogador jogador2 = new Jogador("Pelé");
+        Time time = new Time("Brasil");
+        Jogador[] jogadores = new Jogador[]{jogador1, jogador2};
+
+        jogador1.setTime(time);
+        jogador2.setTime(time);
+
+        time.setJogadores(jogadores);
+
+        System.out.println("---- Jogador ----");
+        jogador1.imprime();
+        jogador2.imprime();
+
+        System.out.println("---- Time ----");
+        time.imprime();
+    }
+}

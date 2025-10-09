f0f98098c957b9b0503881d4fb710af1d6d79cdf
niltinfilho <niltonfilho115@gmail.com>
2025-10-09 11:44:10 -0300

    entendendo o funcionamento do polimorfismo

---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/teste/ProdutoTeste02.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.teste;
+
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Produto;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
+
+public class ProdutoTeste02 {
+    public static void main(String[] args) {
+        Produto produto = new Computador("Macbook pro m2", 20000);
+        System.out.println(produto.getNome());
+        System.out.println(produto.getValor());
+        System.out.println(produto.calcularImposto());
+
+        System.out.println("-----------------------");
+
+        Produto produto2 = new Tomate("Americano", 20);
+        System.out.println(produto2.getNome());
+        System.out.println(produto2.getValor());
+        System.out.println(produto2.calcularImposto());
+        // produto2.getDataValidade(); -> o tipo Produto nao sabe que tomate tem essa funcionalidade
+    }
+}

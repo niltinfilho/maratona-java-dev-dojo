d8dbb5f10bdd4579427beafc431aeca8ddae018b
niltinfilho <niltonfilho115@gmail.com>
2025-10-09 12:44:58 -0300

    entendendo parametros polimórficos

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/teste/ProdutoTeste01.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/teste/ProdutoTeste01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Npolimorfismo.teste;
 
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Televisao;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
 
@@ -8,8 +9,12 @@ public class ProdutoTeste01 {
     public static void main(String[] args) {
         Computador computador = new Computador("Macbook air m4", 12000);
         Tomate tomate = new Tomate("Tomate vermelho", 10);
-        CalculadoraImposto.calcularImpostoComputador(computador);
+        Televisao tv = new Televisao("Samsung 50\"", 5000);
+
+        CalculadoraImposto.calcularImposto(computador);
         System.out.println("_-----------------");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("_-----------------");
+        CalculadoraImposto.calcularImposto(tv);
     }
 }

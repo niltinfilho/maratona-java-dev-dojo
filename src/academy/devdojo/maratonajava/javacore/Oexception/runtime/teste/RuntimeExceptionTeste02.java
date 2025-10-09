c273d88f3e351440f32aab5d4e082c9d55f1b81b
niltinfilho <niltonfilho115@gmail.com>
2025-10-09 14:16:46 -0300

    lançando exceção unchecked

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/teste/RuntimeExceptionTeste02.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.teste;
+
+public class RuntimeExceptionTeste02 {
+    public static void main(String[] args) {
+        System.out.println(divisao(1, 0));
+    }
+
+    private static int divisao(int a, int b) throws IllegalArgumentException {
+        if (b == 0) {
+            throw new IllegalArgumentException("Argumento ilegal, não pode ser 0");
+        }
+        return a / b;
+    }
+}

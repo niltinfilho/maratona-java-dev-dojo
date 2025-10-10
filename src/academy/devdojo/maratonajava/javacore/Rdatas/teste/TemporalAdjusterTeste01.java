bab8ecba5f7da08bfc9ce8985110f676aec6887d
niltinfilho <niltonfilho115@gmail.com>
2025-10-10 14:29:40 -0300

    entendendo classes utilitarias - TemporalAdjuster

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/teste/TemporalAdjusterTeste01.java
@@ -0,0 +1,36 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.teste;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.*;
+
+class ObterProximoDiaUtil implements TemporalAdjuster {
+
+    @Override
+    public Temporal adjustInto(Temporal temporal) {
+        DayOfWeek dayOfWeek = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
+        int addDays;
+        switch (dayOfWeek) {
+            case FRIDAY:
+                addDays = 3;
+                break;
+            case SATURDAY:
+                addDays = 2;
+            default:
+                addDays = 1;
+        }
+        return temporal.plus(addDays, ChronoUnit.DAYS);
+    }
+}
+
+public class TemporalAdjusterTeste01 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(new ObterProximoDiaUtil());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+    }
+}

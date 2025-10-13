8577be91693aeba340b58d27eb878b0b9778af57
niltinfilho <niltonfilho115@gmail.com>
2025-10-13 10:23:41 -0300

    entendendo classes utilitarias - IO - File

---
+++ src/academy/devdojo/maratonajava/javacore/Vio/teste/FileTeste01.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.Vio.teste;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+public class FileTeste01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try {
+            boolean isCreated = file.createNewFile();
+            System.out.println("Created: " + isCreated);
+            System.out.println("Path: " + file.getPath());
+            System.out.println("Absolute Path: " + file.getAbsolutePath());
+            System.out.println("Is Directory: " + file.isDirectory());
+            System.out.println("Is File: " + file.isFile());
+            System.out.println("Is Hidden: " + file.isHidden());
+            System.out.println("Last Modified: " + new Date(file.lastModified()));
+            boolean exists = file.exists();
+            if (exists) {
+                System.out.println("Deleted: " + file.delete());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

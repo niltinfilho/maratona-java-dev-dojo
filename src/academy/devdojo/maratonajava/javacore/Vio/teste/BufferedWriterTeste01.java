687a0a39ec3ed02d9ed937e7b4d0bb0b4d887a48
niltinfilho <niltonfilho115@gmail.com>
2025-10-13 10:51:47 -0300

    entendendo classes utilitarias - IO - BufferedReader

--- src/academy/devdojo/maratonajava/javacore/Vio/teste/BufferedWriterTeste01.java
+++ src/academy/devdojo/maratonajava/javacore/Vio/teste/BufferedWriterTeste01.java
@@ -9,10 +9,10 @@ public class BufferedWriterTeste01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try (FileWriter fw = new FileWriter(file, true);
-             BufferedWriter br = new BufferedWriter(fw)) {
-            br.write("O DevDojo é o melhor curso de Java do Brasil");
-            br.newLine();
-            br.flush();
+             BufferedWriter bw = new BufferedWriter(fw)) {
+            bw.write("O DevDojo é o melhor curso de Java do Brasil");
+            bw.newLine();
+            bw.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }

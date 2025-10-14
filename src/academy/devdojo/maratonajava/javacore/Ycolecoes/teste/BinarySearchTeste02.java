f1cd8d2905655af562b127f671a7340aecb7138d
niltinfilho <niltonfilho115@gmail.com>
2025-10-14 13:58:34 -0300

    entendendo coleções - binarySearch

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/teste/BinarySearchTeste02.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.teste;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BinarySearchTeste02 {
+    public static void main(String[] args) {
+        List<Manga> mangas = new ArrayList<>(6);
+        mangas.add(new Manga(5L, "Hellsing Ultimate", 19.9));
+        mangas.add(new Manga(1L, "Berserk", 9.5));
+        mangas.add(new Manga(4L, "Pokemon", 3.2));
+        mangas.add(new Manga(3L, "Attack on titan", 11.20));
+        mangas.add(new Manga(2L, "Dragon ball z", 2.99));
+
+//        Collections.sort(mangas); // esta ordenando por nome
+        MangaByIdComparator mangaByIdComparator = new MangaByIdComparator();
+        mangas.sort(mangaByIdComparator);
+
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
+
+        Manga mangaToSearch = new Manga(2L, "Dragon ball z", 2.99);
+
+        System.out.println(Collections.binarySearch(mangas, mangaToSearch, mangaByIdComparator));
+    }
+}

7ab5feccb660bcaedfd6907a4b6fdb7abda3b910
niltinfilho <niltonfilho115@gmail.com>
2025-10-14 14:26:41 -0300

    entendendo coleções - Set e HashSet

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/teste/IteratorTeste01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/teste/IteratorTeste01.java
@@ -4,11 +4,13 @@ import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class IteratorTeste01 {
     public static void main(String[] args) {
-        List<Manga> mangas = new ArrayList<>(6);
+//        List<Manga> mangas = new ArrayList<>(6);
+        List<Manga> mangas = new LinkedList<>(); // linked list é mais rapido
         mangas.add(new Manga(5L, "Hellsing Ultimate", 19.9, 0));
         mangas.add(new Manga(1L,"Berserk", 9.5, 5));
         mangas.add(new Manga(4L,"Pokemon", 3.2, 0));

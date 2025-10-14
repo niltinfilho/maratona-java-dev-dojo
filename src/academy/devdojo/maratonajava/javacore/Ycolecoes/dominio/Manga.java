6278a5e1c6e0039e28899a884fe18283681f765b
niltinfilho <niltonfilho115@gmail.com>
2025-10-14 11:24:40 -0300

    entendendo coleções - Sorting lists - Comparable

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Manga.java
@@ -0,0 +1,84 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.dominio;
+
+import java.util.Objects;
+
+public class Manga implements Comparable<Manga> {
+    private Long id;
+    private String nome;
+    private double preco;
+
+    public Manga(Long id, String nome, double preco) {
+        Objects.requireNonNull(id, "Id não pode ser null");
+        Objects.requireNonNull(nome, "Nome não pode ser null");
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Manga manga = (Manga) o;
+        return Double.compare(preco, manga.preco) == 0 && Objects.equals(id, manga.id) && Objects.equals(nome, manga.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, preco);
+    }
+
+    @Override
+    public String toString() {
+        return "Manga{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Manga outroManga) {
+        // negativo se this < outroManga
+        // igual a 0 se this == outroManga
+        // positivo se this > outroManga
+
+//        if (this.id < outroManga.getId()) {
+//            return -1;
+//        } else if (this.id.equals(outroManga.getId())) {
+//            return 0;
+//        } else {
+//            return 1;
+//        }
+
+//        return this.id.compareTo(outroManga.getId()); // pelo id ->  nao seria possivel com tipos primitivos, pois o id é um wrapper
+//        return Double.valueOf(preco).compareTo(outroManga.getPreco()); // pelo preco -> possível solucao para tipos primitivos
+//        return Double.compare(preco, outroManga.getPreco()); // pelo preco -> outra solucao para tipos primitivos
+        return this.nome.compareTo(outroManga.getNome()); // pelo nome
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/teste/MangaSortTeste01.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.teste;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MangaSortTeste01 {
+    public static void main(String[] args) {
+        List<Manga> mangas = new ArrayList<>(6);
+        mangas.add(new Manga(5L, "Hellsing Ultimate", 19.9));
+        mangas.add(new Manga(1L,"Berserk", 9.5));
+        mangas.add(new Manga(4L,"Pokemon", 3.2));
+        mangas.add(new Manga(3L,"Attack on titan", 11.20));
+        mangas.add(new Manga(2L,"Dragon ball z", 2.99));
+
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
+
+        System.out.println("---------------------------");
+        Collections.sort(mangas);
+
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
+
+    }
+}

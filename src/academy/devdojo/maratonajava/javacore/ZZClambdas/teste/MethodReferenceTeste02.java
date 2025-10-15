aaffdd268b4c70329160cfa358ad1d7836e130d5
niltinfilho <niltonfilho115@gmail.com>
2025-10-15 14:36:38 -0300

    entendendo Method Reference - Referência a métodos estáticos

---
+++ src/academy/devdojo/maratonajava/javacore/ZZClambdas/teste/MethodReferenceTeste02.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.ZZClambdas.teste;
+
+import academy.devdojo.maratonajava.javacore.ZZClambdas.dominio.Anime;
+import academy.devdojo.maratonajava.javacore.ZZClambdas.service.AnimeComparators;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+
+public class MethodReferenceTeste02 {
+    public static void main(String[] args) {
+        List<Anime> animeList = new ArrayList<>(List.of(new Anime("Berserk", 43), new Anime("One Piece", 900), new Anime("Naruto", 500)));
+//        Collections.sort(animeList, (a1, a2) -> a1.getTitle().compareTo(a2.getTitle()));
+        animeList.sort(AnimeComparators::compareByTitle);
+        animeList.sort(AnimeComparators::compareByEpisodes);
+        System.out.println(animeList);
+    }
+}

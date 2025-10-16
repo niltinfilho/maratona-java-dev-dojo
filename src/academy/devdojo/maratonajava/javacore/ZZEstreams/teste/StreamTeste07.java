955a2c26f4107b027b64bc4f8912a5f297a6a515
niltinfilho <niltonfilho115@gmail.com>
2025-10-16 10:50:16 -0300

    entendendo Streams - Reduce

---
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/teste/StreamTeste07.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.javacore.ZZEstreams.teste;
+
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.LightNovel;
+
+import java.util.List;
+import java.util.stream.DoubleStream;
+
+public class StreamTeste07 {
+    public static void main(String[] args) {
+        lightNovels.stream()
+                .map(LightNovel::getPrice)
+                .filter(price -> price > 3)
+                .reduce(Double::sum)
+                .ifPresent(System.out::println);
+
+        double sum = lightNovels.stream()
+                .mapToDouble(LightNovel::getPrice)
+                .filter(price -> price > 3)
+                .sum();
+    }
+
+    private static List<LightNovel> lightNovels = List.of(
+            new LightNovel("Tensei Shittara", 8.99),
+            new LightNovel("Overlord", 10.99),
+            new LightNovel("Violet Evergarden", 5.99),
+            new LightNovel("No Game no Life", 2.99),
+            new LightNovel("Fullmetal Alchemist", 5.99),
+            new LightNovel("Kumo Desuga", 1.99),
+            new LightNovel("Kumo Desuga", 1.99),
+            new LightNovel("Monogatari", 4.00)
+    );
+}

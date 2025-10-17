81d49ff3f4ad5e7a15f29e0dbb06ca61da4a0c17
niltinfilho <niltonfilho115@gmail.com>
2025-10-17 13:25:08 -0300

    entendendo jdbc - lombok e log4j

--- src/academy/devdojo/maratonajava/javacore/ZZIjdbc/teste/ConnectionFactoryTeste01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZIjdbc/teste/ConnectionFactoryTeste01.java
@@ -1,13 +1,11 @@
 package academy.devdojo.maratonajava.javacore.ZZIjdbc.teste;
 
-import academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
 
 public class ConnectionFactoryTeste01 {
     public static void main(String[] args) {
-        Producer producer = Producer.ProducerBuilder.builder()
-                .name("NHK").build();
+        Producer producer = Producer.builder().name("Studio Deen").build();
         ProducerRepository.save(producer);
     }
 }
